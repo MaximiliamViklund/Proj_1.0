1004b08c5995c0d8684a3295c073d6b39de296a7
Maximiliam Viklund <maximiliam.viklund@elev.ga.ntig.se>
2023-08-31 10:30:22 +0200

    jhibfdjhbfdkjbh

---
+++ Proj_1/Program.cs
@@ -0,0 +1,27 @@
+﻿string name = "";
+string mood = "";
+string resp = "";
+
+Console.WriteLine("What is your name?");
+name = Console.ReadLine();
+
+Console.WriteLine($"Hello {name}, how are you today?");
+mood = Console.ReadLine();
+
+Console.WriteLine($"Well, I am sorry/happy to hear that you are {mood}.");
+Console.WriteLine("My creator didnt code in multiple responses I'm afraid");
+Console.WriteLine("Do you judge him for that?");
+Console.WriteLine("y/n");
+resp = Console.ReadLine();
+
+if(resp == "y"){
+    Console.WriteLine("Well go fuck yourself, I have a life to live!");
+}
+else if(resp == "n"){
+    Console.WriteLine("Cheers mate, I don't have the energy for this atm.");
+}
+else{
+    Console.WriteLine("That was a yes or no question dumbass!");
+}
+Console.WriteLine("Press ENTER to close");
+Console.ReadLine();
